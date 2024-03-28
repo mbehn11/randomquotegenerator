4d2b249410bbbecfbc77ad414190f77a47f888a2
mbehn11 <mbehn@mail.endicott.edu>
2024-03-28 13:09:50 -0400

    randomquotegenerator

--- js/script.js
+++ js/script.js
@@ -30,5 +30,4 @@ function generateQuote() {
 window.onload = function () {
     generateQuote();
 }
-
-//document.getElementById("generate").addEventListener('click', generateQuote());
+document.getElementById("generate").addEventListener('click', generateQuote);
